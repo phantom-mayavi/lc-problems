0bfbd8afe791e0e8e7d0f45496879d59a0adfef6
phantom-mayavi <phantom.mayavi@gmail.com>
2025-05-25 20:32:17 -0400

    1295: find number of elements in array with even digits

--- src/NumWithEvenDigits_1295.java
+++ src/NumWithEvenDigits_1295.java
@@ -3,6 +3,7 @@ import java.util.stream.IntStream;
 public class NumWithEvenDigits_1295 {
     public static void main(String[] args) {
         NumWithEvenDigits_1295 myObj = new NumWithEvenDigits_1295();
+        System.out.println((int) Math.floor(Math.log10(345)));
         System.out.println(myObj.findNumbers1(new int[]{12,345,2,6,7896}));
         System.out.println(myObj.findNumbers1(new int[]{555,901,482,1771}));
     }
