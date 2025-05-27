b02abe267f2ab82422fcb9abdfdb7469a7fab54d
phantom-mayavi <phantom.mayavi@gmail.com>
2025-05-26 21:25:06 -0400

    1: Two Sum

--- src/TwoSum_1.java
+++ src/TwoSum_1.java
@@ -15,6 +15,19 @@ class TwoSum_1 {
         throw new IllegalArgumentException("No two sum solution");
     }
 
+    public int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> elementsPositions = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (elementsPositions.containsKey(target - nums[i])) {
+                return new int[]{elementsPositions.get(target - nums[i]), i};
+            } else {
+                elementsPositions.put(nums[i], i);
+            }
+        }
+
+        return new int[]{0,0};
+    }
+
     public static void main(String[] args) {
         int[] position = twoSum(new int[]{7,11,2,15}, 9);
         System.out.println(position[0] + " " + position[1]);
